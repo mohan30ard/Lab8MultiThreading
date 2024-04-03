e6e2995504f8476010bc8d1b3f54c83be307f838
Mohan <n01620006@humber.ca>
2024-04-03 18:45:30 -0400

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Generate = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(55, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 38);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Generate
+            // 
+            this.Generate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Generate.Location = new System.Drawing.Point(271, 47);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(196, 54);
+            this.Generate.TabIndex = 1;
+            this.Generate.Text = "&Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(55, 126);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(294, 404);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Generate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 562);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Generate_Click(object sender, EventArgs e)
+        {
+            if(!int.TryParse(textBox1.Text, out int n))
+            {
+                MessageBox.Show("Invalid input","Invalid number input",MessageBoxButtons.OK);
+                return;
+            }
+            Task generateTask = Task.Run(() => GenerateFibonacci(n));
+
+            await generateTask;
+
+            listBox1.Items.Clear();
+        }
+
+        public async void GenerateFibonacci(int n)
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                AddToFibonacciListBox(c);
+
+
+                await Task.Delay(100);
+            }
+        }
+
+        public void AddToFibonacciListBox(int value)
+        {
+            Invoke(new Action(() =>
+            {
+                listBox1.Items.Add(value);
+            }));
+        }
+
+    }
+}
